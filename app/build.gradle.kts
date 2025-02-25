05bb875fdb2b29d464c62629fddfc7d53b4ab36f
mohamed.morse <mohamed.morse@expertapps.com.sa>
2025-02-25 20:12:26 +0200

    update minimum sdk to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.morse.moviesmagic"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
